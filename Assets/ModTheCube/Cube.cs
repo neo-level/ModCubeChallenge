187af6f4a9db1e5b84fe41a8856010ec0665e352
frederik desmet <info@neo-level.com>
2021-01-14 16:34:17 +0100

    Addition of movement over a time base and scale increment.

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 /*
- * TODO: Change the cube's location (transform)
- * TODO: Change the cube's scale
  * TODO: Change the angle at which the cube rotates
  * TODO: Change the cube’s rotation speed
  * TODO: Change the cube’s material color
@@ -16,19 +15,26 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
+    private float speed = 1.0f;
+    private Vector3 scaleChange = new Vector3(0.001f, 0.001f, 0.001f);
     
+
+
     void Start()
     {
         transform.position = new Vector3(3, 4, 1);
         transform.localScale = Vector3.one * 1.3f;
-        
+
         Material material = Renderer.material;
-        
+
         material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
     }
-    
+
     void Update()
     {
         transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Translate(Vector3.right * (Time.deltaTime * speed));
+        transform.localScale += scaleChange;
+        
     }
 }
