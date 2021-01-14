7df46df7fa62982ba84a37bd283ba4d547739ef6
frederik desmet <info@neo-level.com>
2021-01-14 16:04:37 +0100

    Initial setup with todos on challenge.

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * TODO: Change the cube's location (transform)
+ * TODO: Change the cube's scale
+ * TODO: Change the angle at which the cube rotates
+ * TODO: Change the cube’s rotation speed
+ * TODO: Change the cube’s material color
+ * TODO: Change the cube’s material opacity
+ * ===== Bonus.
+ * TODO: Modify any of the changes above so they change randomly each time the scene is played.
+ * TODO: Add extra functionality to the cube. For example, how might you change the color of the cube over time?
+ * 
+ */
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 1.3f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
