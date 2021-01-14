084bed0abd7176b4880de44ff9459c9b16fe6bbf
frederik desmet <info@neo-level.com>
2021-01-14 17:25:26 +0100

    add script that camera follows the Cubes. finished some of the todos

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
- * TODO: Change the angle at which the cube rotates
- * TODO: Change the cube’s rotation speed
- * TODO: Change the cube’s material color
- * TODO: Change the cube’s material opacity
  * ===== Bonus.
  * TODO: Modify any of the changes above so they change randomly each time the scene is played.
  * TODO: Add extra functionality to the cube. For example, how might you change the color of the cube over time?
@@ -15,9 +11,11 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    private float speed = 1.0f;
-    private Vector3 scaleChange = new Vector3(0.001f, 0.001f, 0.001f);
-    
+    private float _speed = 1.0f;
+    private float _colorChangeSpeed = 1.0f;
+    private float _timestamp;
+    private float _colorTimestamp;
+    private Vector3 _scaleChange = new Vector3(0.1f, 0.1f, 0.1f);
 
 
     void Start()
@@ -32,9 +30,29 @@ public class Cube : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
-        transform.Translate(Vector3.right * (Time.deltaTime * speed));
-        transform.localScale += scaleChange;
-        
+        Material material = Renderer.material;
+
+
+        // Rotates the cube over time.
+        transform.Rotate(0.0f, 10.0f * Time.deltaTime * _speed, 0.0f);
+
+        // Moves the cube over time.
+        transform.Translate(Vector3.right * (Time.deltaTime * _speed));
+
+        // Scales the cube over time.
+        if (Time.time >= _timestamp)
+        {
+            transform.localScale += _scaleChange;
+            _timestamp = Time.time + (_speed / 10);
+        }
+
+        if (Time.time >= _colorTimestamp)
+        {
+            // pick a random color
+            Color newColor = new Color(Random.value, Random.value, Random.value, Random.value);
+            // apply it on current object's material
+            material.color = newColor;
+            _colorTimestamp = Time.time + _colorChangeSpeed;
+        }
     }
 }
---
+++ Assets/Scripts/FollowCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCube : MonoBehaviour
+
+{
+    public GameObject cube;
+    private Vector3 _offset = new Vector3(0, 20, -100);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cube.transform.position + _offset;
+    }
+}
